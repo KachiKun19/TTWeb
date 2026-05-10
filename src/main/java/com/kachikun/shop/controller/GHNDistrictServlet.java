3008490baf0dbf705f2ecc9e35e884a8c839cbf6
23130312-star <23130312@st.hcmuaf.edu.vn>
2026-05-10 23:12:09 +0700

    [ADD]: Gọi GHN API để lấy danh sách phường/xã theo quận đã chọn

---
+++ src/main/java/com/kachikun/shop/controller/GHNDistrictServlet.java
@@ -0,0 +1,60 @@
+package com.kachikun.shop.controller;
+
+import com.kachikun.shop.utils.GHNConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@WebServlet("/ghn-districts")
+public class GHNDistrictServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String provinceId = request.getParameter("provinceId");
+
+        URL url = new URL("https://online-gateway.ghn.vn/shiip/public-api/master-data/district");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        conn.setRequestProperty("Token", GHNConfig.TOKEN);
+
+        String json = """
+                {
+                    "province_id": %s
+                }
+                """.formatted(provinceId);
+
+        OutputStream os = conn.getOutputStream();
+        os.write(json.getBytes());
+        os.flush();
+        os.close();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+
+        response.setContentType("application/json");
+        response.getWriter().write(sb.toString());
+    }
+}
