0d285407f2da8805c1e204eae27016441941bb9d
23130312-star <23130312@st.hcmuaf.edu.vn>
2026-05-31 00:56:37 +0700

    [FEAT]: Add GoogleConstants cấu hình cho Google OAuth(client ID, secret,...)

---
+++ src/main/java/com/kachikun/shop/utils/GoogleConstants.java
@@ -0,0 +1,8 @@
+package com.kachikun.shop.utils;
+
+public class GoogleConstants {
+    public static final String GOOGLE_CLIENT_ID = System.getenv("GOOGLE_CLIENT_ID");
+    public static final String GOOGLE_CLIENT_SECRET = System.getenv("GOOGLE_CLIENT_SECRET");
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v2/userinfo";
+}
